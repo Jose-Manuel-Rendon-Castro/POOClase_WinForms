c0c4aa23eea976be48e0dd3f59c513d2d9ba0fbb
José Manuel Rendón Castro <jmrendoncas@gmail.com>
2024-11-20 00:47:37 -0700

    Revert "Creación de excepciones para control de errores e implementación de lógica al controlador. Ya funciona"
    This reverts commit ae0f4874b30ed19e64143e5b88234ed8d380815b.

--- POOClase_WinForms/Exceptions/MaxValueLowerThanMinValueException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace POOClase_WinForms.Exceptions
-{
-    public class MaxValueLowerThanMinValueException : Exception
-    {
-        public MaxValueLowerThanMinValueException() : base() { }
-        public MaxValueLowerThanMinValueException(string message) : base(message) { }
-    }
-}
--- POOClase_WinForms/Exceptions/NotParsedException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace POOClase_WinForms.Exceptions
-{
-    public class NotParsedException : Exception
-    {
-        public NotParsedException() : base("No se pudo parsear el valor") { }
-        public NotParsedException(string message) : base(message) { }
-    }
-}
