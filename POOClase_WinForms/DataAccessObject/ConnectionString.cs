8b6ba63a013d7e0bc3ba5dc3b7e62c67e0809a55
José Manuel Rendón Castro <jmrendoncas@gmail.com>
2024-11-19 17:00:52 -0700

    Indicaciones en ConecctionString.cs

--- POOClase_WinForms/DataAccessObject/ConnectionString.cs
+++ POOClase_WinForms/DataAccessObject/ConnectionString.cs
@@ -1,7 +1,8 @@
 ﻿namespace POOClase_WinForms.AccessData
 {
     public abstract class ConnectionString
-    {                                      // Uid =Tu nombre de usuario.     Pwd = Contraseña de tu base de datos.
+    {   // Uid =Tu nombre de usuario.     Pwd = Contraseña de tu base de datos.
+        // Cambiar los valores de Uid y Pwd a los que usted utilize para conectarse con MySql
         protected static readonly string connectionString = "Server=localhost;Database=tienda;Uid=root;Pwd=23040273";
     }
 }
